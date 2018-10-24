f65cc665b2ce03d93cae87dec4d6111174f982f3
esteban <josueayo.ejat@gmail.com>
2018-10-24 12:01:20 -0500

    este es el primer mensaje de hola mundo

---
+++ src/com/eayo/vista/Aplicacion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eayo.vista;
+
+/**
+ *
+ * @author LAB4-PC7
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
