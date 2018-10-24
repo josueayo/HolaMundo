6cf10300c2ef8d80a6091e60b6815cd193594808
esteban <josueayo.ejat@gmail.com>
2018-10-24 12:12:41 -0500

    imlemento la calse punto

---
+++ src/com/eayo/modelo/Punto.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eayo.modelo;
+
+/**
+ *
+ * @author LAB4-PC7
+ */
+public class Punto {
+    private int x;
+    private int y;
+
+    public Punto() {
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public String toString() {
+        return "p (" +getX()+":" +getY()
+    }    
+}
